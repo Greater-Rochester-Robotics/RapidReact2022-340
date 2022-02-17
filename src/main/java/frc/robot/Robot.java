1befa90606af993498fa1b707aa63f3fd86bec87
rob-heslin <Rob.Heslin@gmail.com>
2022-02-17 17:25:35 -0500

    [Rob]Added code to disable LiveWindow, a thing that we don't use and doubles CAN error reporting when devices not found

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -25,6 +26,9 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    //Stop the liveWindow from starting, we don't use it.
+    LiveWindow.setEnabled(false);
+    LiveWindow.disableAllTelemetry();
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     robotContainer = new RobotContainer();
