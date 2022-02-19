1efd54d536d5f47d325d31e7259b7a52a3ca802f
Nathan M <nlm899999@gmail.com>
2022-02-19 18:30:37 -0500

    [Chris, Lydia] Moved code from DriveTurnToAngle over to new DriveTurnToTarget class
    - Code that was meant for DriveTurnToTarget was put in DriveTurnToAngle
    - Reverted DriveTurnToAngle back to what it should be

--- src/main/java/frc/robot/commands/drive/util/DriveTurnToAngle.java
+++ src/main/java/frc/robot/commands/drive/util/DriveTurnToAngle.java
@@ -4,12 +4,10 @@
 
 package frc.robot.commands.drive.util;
 
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj.Timer;
-
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants;
+
 import frc.robot.RobotContainer;
+import frc.robot.Constants;
 
 public class DriveTurnToAngle extends CommandBase {
   private double angle = 0;
@@ -38,22 +36,6 @@ public class DriveTurnToAngle extends CommandBase {
   @Override
   public void execute() {
     RobotContainer.swerveDrive.driveRobotCentric(0, 0, RobotContainer.swerveDrive.getRobotRotationPIDOut(angle), false);
-    boolean isOnTarget = false;//TODO: we need to set this in a method
-    Timer timer = new Timer();
-    double rotateToAngle = 0.0;
-    if(timer.hasElapsed(.2) && !isOnTarget){
-      Translation2d target = RobotContainer.swerveDrive.driveOdometry.getPoseMeters().getTranslation().minus(Constants.FIELD_CENTER);
-      double desiredAngle = Math.atan2(target.getY(), target.getX());
-      double currentAngle = RobotContainer.swerveDrive.getGyroInRad();
-      double absoluteCurrentAngle = currentAngle%Math.PI;
-      if(absoluteCurrentAngle > Math.PI){
-        absoluteCurrentAngle -= 2*(Math.PI);
-      }
-      else if(absoluteCurrentAngle < -Math.PI){
-        absoluteCurrentAngle += 2 * Math.PI;
-      }
-      rotateToAngle = currentAngle - absoluteCurrentAngle + desiredAngle;
-      }
     if(Math.abs(angle - RobotContainer.swerveDrive.getGyroInRad()) < .03){
       onTargetCount++;
     }else{
