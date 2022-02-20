99833c18b304c642d3f5f724fad02b52b8d1c92b
Nathan M <nlm899999@gmail.com>
2022-02-19 20:42:19 -0500

    [Chris, Lydia, Jack] Created DriveTurnToTarget command
    DriveTurnToTarget:
    - Uses limelight to point towards target
    - If limelight doesn't see target turn robot in the general direction of the center of the field

--- src/main/java/frc/robot/commands/drive/DriveTurnToTarget.java
+++ src/main/java/frc/robot/commands/drive/DriveTurnToTarget.java
@@ -7,50 +7,85 @@ package frc.robot.commands.drive;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.XboxController.Axis;
+
 import frc.robot.Constants;
+import frc.robot.Robot;
 import frc.robot.RobotContainer;
 
 public class DriveTurnToTarget extends CommandBase {
-  private double angle = 0;
-  private int onTargetCount;
+  Timer timer = new Timer();
+
   public DriveTurnToTarget() {
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(RobotContainer.swerveDrive, RobotContainer.limeLight);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    RobotContainer.limeLight.setLightState(3);
+    timer.start();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
+  
   @Override
   public void execute() {
-    RobotContainer.swerveDrive.driveRobotCentric(0, 0, RobotContainer.swerveDrive.getRobotRotationPIDOut(angle), false);
-    boolean isOnTarget = false;//TODO: we need to set this in a method
-    Timer timer = new Timer();
-    double rotateToAngle = 0.0;
+    boolean isOnTarget = RobotContainer.limeLight.haveTarget();
+
+    // Default to the current angle of the robot
+    double rotateToAngle = RobotContainer.swerveDrive.getGyroInRad();
+    // Point robot in the general direction of the target if the limelight doesn't see the target
     if(timer.hasElapsed(.2) && !isOnTarget){
+      // Finds where we are relative to the center of the field
       Translation2d target = RobotContainer.swerveDrive.driveOdometry.getPoseMeters().getTranslation().minus(Constants.FIELD_CENTER);
       double desiredAngle = Math.atan2(target.getY(), target.getX());
-      double currentAngle = RobotContainer.swerveDrive.getGyroInRad();
-      double absoluteCurrentAngle = currentAngle%Math.PI;
+
+      // Calculating current angle between -pi and pi
+      double currentAngle = rotateToAngle;
+      double absoluteCurrentAngle = currentAngle%Constants.TWO_PI;
       if(absoluteCurrentAngle > Math.PI){
         absoluteCurrentAngle -= 2*(Math.PI);
       }
       else if(absoluteCurrentAngle < -Math.PI){
         absoluteCurrentAngle += 2 * Math.PI;
       }
+
+      // Calculate the robot's target angle given the continuous angle of the gyroscope
       rotateToAngle = currentAngle - absoluteCurrentAngle + desiredAngle;
-      }
-    if(Math.abs(angle - RobotContainer.swerveDrive.getGyroInRad()) < .03){
-      onTargetCount++;
-    }else{
-      onTargetCount = 0;
     }
+    // Set the rotate to angle to the target if limelight sees it
+    else if(isOnTarget) {
+      rotateToAngle -= Math.toRadians(RobotContainer.limeLight.angleToTarget());
+    }
+
+    // Sets away and lateral speeds using driver axis
+    double  awaySpeed = Robot.robotContainer.getDriverAxis(Axis.kLeftY);
+    double lateralSpeed = Robot.robotContainer.getDriverAxis(Axis.kLeftX);
+    // Check if secondary sticks are being used
+    if(Math.abs(Robot.robotContainer.getDriverAxis(Axis.kRightY))>.1 ||
+       Math.abs(Robot.robotContainer.getDriverAxis(Axis.kRightX))>.1){
+      // If secondary sticks used, replace with secondary sticks witha slow factor
+      awaySpeed = Robot.robotContainer.getDriverAxis(Axis.kRightY)*.5;
+      lateralSpeed = Robot.robotContainer.getDriverAxis(Axis.kRightX)*.5;
+    }
+
+    // Drive robot using driver axis and rotateToAngle
+    RobotContainer.swerveDrive.driveFieldRelative(
+      awaySpeed*-Constants.DRIVER_SPEED_SCALE_LINEAR,
+      lateralSpeed*-Constants.DRIVER_SPEED_SCALE_LINEAR,
+      RobotContainer.swerveDrive.getRobotRotationPIDOut(rotateToAngle), 
+      false
+    );
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    RobotContainer.limeLight.setLightState(1);
+    RobotContainer.swerveDrive.stopAllModules();
+  }
 
   // Returns true when the command should end.
   @Override
